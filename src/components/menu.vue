c18ba0fe575e73310ea0ddc31b77f214d7283c79
Bin <1204443652@qq.com>
2020-12-22 18:13:23 +0800

    second

---
+++ src/components/menu.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-row class="tac" style="height: 100%">
+    <el-col :span="12" class="menu-box">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>菜单一</span>
+          </template>
+          <el-menu-item-group>
+          </el-menu-item-group>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-submenu index="1-2" class="list-item">
+                <template slot="title">选项2</template>
+                <el-menu-item index="1-2-1" class="list-item-child">选项1</el-menu-item>
+                <el-menu-item index="1-2-2" class="list-item-child">选项2</el-menu-item>
+            </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">菜单二</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.menu-box {
+    height: 100%;
+    width: 100%;
+}
+.el-menu {
+    height: 100%;
+}
+.list-item {
+    padding: 0 40px 0 15px;
+}
+.list-item-child {
+    padding: 0!important;
+    min-width: 180px;
+}
+</style>
--- src/page/layout.vue
+++ src/page/layout.vue
@@ -1,38 +1,34 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
-    <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-container>
-        <el-main>Main</el-main>
-        <el-footer>Footer</el-footer>
-        </el-container>
-    </el-container>
-    </el-container>
+    <el-aside width="200px">
+      <Menu></Menu>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
 </template>
 
 <script>
-export default {};
+import Menu from "@/components/menu"
+export default {
+  components: {
+    Menu
+  }
+};
 </script>
 
 <style>
-#main {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    flex: 1;
-}
-.el-header {
-  background-color: #b3c0d1;
-  color: #333;
-  line-height: 60px;
+.el-container {
+  width: 100%;
+  height: 100%;
 }
 .el-aside {
+  height: 100%;
+  color: rgb(24, 22, 22);
+  box-sizing: border-box;
+}
+.el-main {
+  /* background-color: #97b1d6; */
+  height: 100%;
   color: #333;
 }
 </style>
